4376f744632954eb39a33615246461400ae406e2
Viniciuswz <vinicius-araujo0@hotmail.com>
2020-12-27 19:57:01 -0300

    feature(design pattern): eslint, prettier and editorconfig

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import { View, Text } from 'react-native';
 
 const App: React.FC = () => {
-    return (
-        <View>
-            <Text>Olá mundo</Text>
-        </View>
-    );
+  return (
+    <View>
+      <Text>Olá mundo</Text>
+    </View>
+  );
 };
 
 export default App;
