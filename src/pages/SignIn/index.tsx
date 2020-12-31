8e5ad0afe80453b9f50eac74da88bfd535937653
Viniciuswz <vinicius-araujo0@hotmail.com>
2020-12-31 17:11:16 -0300

    feature(SignIn): details

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,6 +1,23 @@
 import React from 'react';
 
-import { Container, Title, Image } from './styles';
+import {
+  Image,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+  View,
+} from 'react-native';
+
+import Icon from 'react-native-vector-icons/Feather';
+
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
 import logoImg from '../../assets/logo.png';
 
 import Input from '../../components/Input';
@@ -8,20 +25,51 @@ import Button from '../../components/Button';
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logoImg} />
-      <Title>Faça seu logon</Title>
-
-      <Input name="email" icon="mail" placeholder="E-mail" />
-      <Input name="password" icon="lock" placeholder="Password" />
-      <Button
-        onPress={() => {
-          console.log('jaca');
-        }}
+    <>
+      <KeyboardAvoidingView
+        style={{ flexGrow: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        keyboardVerticalOffset={500}
+        enabled
       >
-        Entrar
-      </Button>
-    </Container>
+        <ScrollView
+          contentContainerStyle={{ flexGrow: 1 }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <Container>
+            <Image source={logoImg} />
+            <View>
+              <Title>Faça seu logon</Title>
+            </View>
+
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Password" />
+            <Button
+              onPress={() => {
+                console.log('jaca');
+              }}
+            >
+              Entrar
+            </Button>
+            <ForgotPassword
+              onPress={() => {
+                console.log('jaca');
+              }}
+            >
+              <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+          <CreateAccountButton
+            onPress={() => {
+              console.log('jaca');
+            }}
+          >
+            <Icon name="log-in" size={20} color="#fd9000" />
+            <CreateAccountButtonText>Criar conta</CreateAccountButtonText>
+          </CreateAccountButton>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </>
   );
 };
 
